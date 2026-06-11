8eb0a08b566fdc146cafa17b8e891b7a839e3025
eceelifk <kavakelif756@gmail.com>
2026-06-11 22:48:39 +0300

    Update arayüz_proje.py

--- arayüz_proje.py
+++ arayüz_proje.py
@@ -1,61 +1,63 @@
 # =================================================================
-# Proje: Basit Bir Lexical Analyzer 
+# Ad Soyad: Elif Kavak
+# Öğrenci No: 231201024
+# Proje: Lexical Analyzer 
 # =================================================================
 
 import tkinter as tk
 
 def lexical_analyzer():
-    """Hocanın tüm kurallarını içeren senin kod mantığın."""
-    # 1. Anahtar kelimeler (Keywords) 
-    keywords = ["if", "else", "while", "for", "return", "int", "float", "string", "class", "break"]
+    
+    # anahtar k
+    keywords = ["if", "else", "while", "for", "return", "int", "float", "string", "class", "break", "continue","switch","case","default"]
     
 
     user_input = entry.get().strip()
 
-    # KURAL: Boş kontrol
+    # Boş mu k.
     if not user_input or " " in user_input:
         result_label.config(text="Geçerli bir değişken ismi değildir.", fg="red")
         return
     
-    # KURAL: Harf ile başlamalıdır (a-z veya A-Z) 
+    # harf ile başlıyor mu (a-z veya A-Z) 
     if not user_input[0].isalpha():
         result_label.config(text="Geçerli bir değişken ismi değildir.", fg="red")
         return
     
-    # KURAL: Sadece harf, rakam ve alt çizgi (_) içerebilir 
-    # KURAL: Özel karakterler içeremez (@, #, !, - vb.)
+    # Sadece harf, rakam ve alt çizgi (_) içermesi
+    # özel karakter içeremez 
     for char in user_input:
         if not (char.isalnum() or char == "_"):
             result_label.config(text="Geçerli bir değişken ismi değildir.", fg="red")
             return
         
-    # KURAL: Anahtar kelime (keyword) olmamalıdır 
+    # anahtar kelime olmamalı
     if user_input in keywords:
         result_label.config(text="Geçerli bir değişken ismi değildir.", fg="red")
         return
     
-    # TÜM KURALLAR GEÇİLİRSE 
+    # hepsi sağlandıysa 
     result_label.config(text="Geçerli bir değişken ismidir.", fg="green")
 
-# --- ARAYÜZ AYARLARI (Tkinter) ---
+# --- arayüz ayarları(tkinter)---
 root = tk.Tk()
-root.title("BLG306 - Lexical Analyzer")
-root.geometry("400x250")
+root.title("Lexical Analyzer")
+root.geometry("400x200")
 
-# Bilgi Etiketi 
+# bilgi et.
 tk.Label(root, text="Değişken İsmini Girin", font=("Calibri", 12)).pack(pady=10)
 
-# Giriş Kutusu
+# giriş kutusu
 entry = tk.Entry(root, font=("Calibri", 12), width=25)
 entry.pack(pady=5)
 
-# Analyze Butonu 
+# analyze butonu 
 analyze_button = tk.Button(root, text="Analyze", command=lexical_analyzer, font=("Calibri", 11), bg="#d1d1d1")
 analyze_button.pack(pady=20)
 
-# Sonuç Yazısı 
+# sonuç yazısı
 result_label = tk.Label(root, text="", font=("Calibri", 11, "bold"))
 result_label.pack(pady=10)
 
-# Programı Çalıştır
+# programı Çalıştırmak icin
 root.mainloop()
